1d17bd8ccde308b21e85427f9098157cc31f3273
ebenezerdon <ebenezerdonu@gmail.com>
2022-06-06 15:54:43 +0100

    add edit bio feature

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,17 +1,32 @@
+import { useState } from 'react'
 import profileIcon from '../assets/profileIcon.svg'
 
 const Bio = () => {
+  const [nameOfUser, setNameOfUser] = useState('Ebenezer Don')
+  const [aboutUser, setAboutUser] = useState('Building Newdev.io - Learn to code and connect with the best minds.')
+  const [editFormIsOpen, setEditFormIsOpen] = useState(false)
+  // explain useEffect hook. setFormIsOpen to true by default as an example
+
+  const updateUserDetails = (e) => {
+    e.preventDefault()
+    setNameOfUser(e.target.nameOfUser.value)
+    setAboutUser(e.target.aboutUser.value)
+    setEditFormIsOpen(false)
+  }
+
   const editForm = (
-    <form className="edit-bio-form">
-      <input type="text" id="" placeholder="Your name" />
-      <input type="text" id="" placeholder="About you" />
+    <form className="edit-bio-form" onSubmit={(e) => updateUserDetails(e)}>
+      <input type="text" id="" name="nameOfUser" defaultValue={nameOfUser} placeholder="Your name" required />
+      <input type="text" id="" name="aboutUser" defaultValue={aboutUser} placeholder="About you" required />
       <br />
-      <button type="button" className="cancel-button">
+      <button type="button" className="cancel-button" onClick={() => setEditFormIsOpen(false)}>
         Cancel
       </button>
       <button type="submit">Save</button>
     </form>
   )
+
+  const editButton = <button onClick={() => setEditFormIsOpen(true)}>Edit</button>
 
   return (
     <section className="bio">
@@ -19,10 +34,10 @@ const Bio = () => {
         <img src={profileIcon} alt="profile" />
       </div>
       <div className="profile-info">
-        <p className="name">Ebenezer Don</p>
-        <p className="about">Building Newdev.io - Learn to code and connect with the best minds.</p>
-        <button>Edit</button>
-        {editForm}
+        <p className="name">{nameOfUser}</p>
+        <p className="about">{aboutUser}</p>
+
+        {editFormIsOpen ? editForm : editButton}
       </div>
     </section>
   )
