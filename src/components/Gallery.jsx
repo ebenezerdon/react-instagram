88cb071257a4dfa2d30bf10d892737b748d377b5
ebenezerdon <ebenezerdonu@gmail.com>
2022-06-06 17:23:21 +0100

    add photo to gallery feature

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,36 +1,38 @@
+import getPhotoUrl from 'get-photo-url'
+import { useState } from 'react'
+
 const Gallery = () => {
+  const [allPhotos, setAllPhotos] = useState([])
+
+  const addPhoto = async () => {
+    const newPhoto = {
+      id: Date.now(),
+      url: await getPhotoUrl('#addPhotoInput'),
+    }
+
+    setAllPhotos([newPhoto, ...allPhotos])
+  }
+
+  const removePhoto = (id) => {
+    setAllPhotos(allPhotos.filter((photo) => photo.id !== id))
+  }
+
   return (
     <>
-      <input type="file" name="photo" id="addPhotoInput" />
-      <label htmlFor="addPhotoInput">
+      <input type="file" accept="image/*" name="photo" id="addPhotoInput" />
+      <label htmlFor="addPhotoInput" onClick={addPhoto}>
         <i className="add-photo-button fas fa-plus-square" />
       </label>
 
       <section className="gallery">
-        <div className="item">
-          <img src="https://images.pexels.com/photos/4835026/pexels-photo-4835026.jpeg" className="item-image" alt="" />
-          <button className="delete-button">Delete</button>
-        </div>
-
-        <div className="item">
-          <img src="https://images.pexels.com/photos/4940300/pexels-photo-4940300.jpeg" className="item-image" alt="" />
-          <button className="delete-button">Delete</button>
-        </div>
-
-        <div className="item">
-          <img src="https://images.pexels.com/photos/1719344/pexels-photo-1719344.jpeg" className="item-image" alt="" />
-          <button className="delete-button">Delete</button>
-        </div>
-
-        <div className="item">
-          <img src="https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg" className="item-image" alt="" />
-          <button className="delete-button">Delete</button>
-        </div>
-
-        <div className="item">
-          <img src="https://images.pexels.com/photos/4918481/pexels-photo-4918481.jpeg" className="item-image" alt="" />
-          <button className="delete-button">Delete</button>
-        </div>
+        {allPhotos.map((photo) => (
+          <div className="item" key={photo.id}>
+            <img src={photo.url} className="item-image" alt="" />
+            <button className="delete-button" onClick={() => removePhoto(photo.id)}>
+              Delete
+            </button>
+          </div>
+        ))}
       </section>
     </>
   )
