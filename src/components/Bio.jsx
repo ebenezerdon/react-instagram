b1fcc1ee6cad19a18ccf469edb193d8c3ce28d0a
ebenezerdon <ebenezerdonu@gmail.com>
2022-06-06 14:36:28 +0100

    add edit bio form

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,6 +1,18 @@
 import profileIcon from '../assets/profileIcon.svg'
 
 const Bio = () => {
+  const editForm = (
+    <form className="edit-bio-form">
+      <input type="text" id="" placeholder="Your name" />
+      <input type="text" id="" placeholder="About you" />
+      <br />
+      <button type="button" className="cancel-button">
+        Cancel
+      </button>
+      <button type="submit">Save</button>
+    </form>
+  )
+
   return (
     <section className="bio">
       <div className="profile-photo">
@@ -10,6 +22,7 @@ const Bio = () => {
         <p className="name">Ebenezer Don</p>
         <p className="about">Building Newdev.io - Learn to code and connect with the best minds.</p>
         <button>Edit</button>
+        {editForm}
       </div>
     </section>
   )
