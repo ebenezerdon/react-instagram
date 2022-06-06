5e3a14e7b59891489aacee783aff38a3152731ba
ebenezerdon <ebenezerdonu@gmail.com>
2022-06-06 13:30:32 +0100

    directly use images in gallery
    - also remove redundant galleryNav div/styling

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,20 +1,36 @@
-import { posts } from '../data'
-
 const Gallery = () => {
   return (
     <>
       <input type="file" name="photo" id="addPhotoInput" />
       <label htmlFor="addPhotoInput">
-        <i className="add-photo fas fa-plus-square" />
+        <i className="add-photo-button fas fa-plus-square" />
       </label>
-      <div className="gallery-nav" />
+
       <section className="gallery">
-        {posts.map((post) => (
-          <div className="item" key={post.id} tabIndex={0}>
-            <img src={post.imageUrl} className="item-image" alt="" />
-            <button className="delete-button">Delete</button>
-          </div>
-        ))}
+        <div className="item">
+          <img src="https://images.pexels.com/photos/4835026/pexels-photo-4835026.jpeg" className="item-image" alt="" />
+          <button className="delete-button">Delete</button>
+        </div>
+
+        <div className="item">
+          <img src="https://images.pexels.com/photos/4940300/pexels-photo-4940300.jpeg" className="item-image" alt="" />
+          <button className="delete-button">Delete</button>
+        </div>
+
+        <div className="item">
+          <img src="https://images.pexels.com/photos/1719344/pexels-photo-1719344.jpeg" className="item-image" alt="" />
+          <button className="delete-button">Delete</button>
+        </div>
+
+        <div className="item">
+          <img src="https://images.pexels.com/photos/4006576/pexels-photo-4006576.jpeg" className="item-image" alt="" />
+          <button className="delete-button">Delete</button>
+        </div>
+
+        <div className="item">
+          <img src="https://images.pexels.com/photos/4918481/pexels-photo-4918481.jpeg" className="item-image" alt="" />
+          <button className="delete-button">Delete</button>
+        </div>
       </section>
     </>
   )
